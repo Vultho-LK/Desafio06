087a32daf537d90cffdc40fa4c237a3717a8ff4e
Hemerson Luan <140278282+Vultho-LK@users.noreply.github.com>
2024-08-04 21:04:05 -0300

    Add files via upload

---
+++ Desafio06.py
@@ -0,0 +1,3 @@
+# mostrar o dobro, triplo e a raiz quadrada de um numero 
+n1 = int(input('digite um numero: '))
+print('o dobro de {} é {} o triplo {} e a raiz quadrada {}'.format(n1, n1 * 2, n1 * 3, n1 **(1/2)))
